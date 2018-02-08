a70e6f4d9385695a34a4ac712656a7d7880e8189
Samuel Chen <samuel.1.chen@nokia-sbell.com>
2018-02-08 17:17:48 +0800

    First travis CI

---
+++ support/bootstrap-docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+ 
+set -eux
+
+PACKAGES="
+findutils
+gcc
+rpm-build
+make
+autoconf
+automake
+libtool
+"
+for p in `echo ${PACKAGES}`; do
+    yum -y install $p
+done
+
+mkdir -p ~/rpmbuild/SOURCES
+
+cd /
+tar -cf ~/rpmbuild/SOURCES/gmssl-${GMSSL_VERSION}.tar gmssl-${GMSSL_VERSION}
+gzip ~/rpmbuild/SOURCES/gmssl-${GMSSL_VERSION}.tar
+rpmbuild -bb /gmssl-${GMSSL_VERSION}/rpm/gmssl.spec
+
+ls -l ~/rpmbuild/RPMS/x86_64/
+
+echo "End"
